40e757faafd2fafc10c0532053cfd448c1671a84
serap <serapp.oral@gmail.com>
2022-05-12 20:51:37 +0300

    uploaded

--- 1-functionDeclaration.js
+++ 1-functionDeclaration.js
@@ -1,7 +1,33 @@
+
+console.log("*********FUNC DECLARATION ***********");
+
+//Örnek 1
+//******************************************************************/
+
+//DECLARATION 
+
+//!Parametre almamış , bir şey döndürmek (void function ) returnsuz functionlara void function demektir.
+
 function yazdır()
 {
     console.log("Merhaba");
 
 }
 
+yazdır(); //!invoke
 yazdır();
+yazdır();
+
+
+console.log(typeof (yazdır)); //*function type
+
+//Örnek2: Parametreli Fonksiyonu
+//******************************* */
+
+function selamla(isim,soyisim=' '){
+    console.log(`Merhaba  ${isim}  ${soyisim}`);
+}
+
+selamla("serap","Yılmaz");
+selamla("Ali");
+selamla("Ayşe");
